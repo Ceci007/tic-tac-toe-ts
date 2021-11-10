21d7a996f7d8529819ce5a4444de7a5f0b4695c0
Ceci007 <cecibenitezca@gmail.com>
2021-11-10 14:02:42 -0300

    Added isTerminal util

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -2,14 +2,24 @@ import React, { ReactElement } from "react";
 import { SafeAreaView } from "react-native";
 import styles from "./single-player-game.styles";
 import { GradientBackground, Board } from "@components";
-import { printFormattedBoard, BoardState, isEmpty, isFull, getAvailableMoves } from "@utils";
+import {
+    printFormattedBoard,
+    BoardState,
+    isEmpty,
+    isFull,
+    getAvailableMoves,
+    isTerminal
+} from "@utils";
 
 export default function Game(): ReactElement {
-    const b: BoardState = ["x", "o", "x", "o", "x", null, "o", "x", null];
+    // prettier-ignore
+    const b: BoardState = [
+        "x", "o", "x", 
+        "x", "o", "x", 
+        "o", "x", "o"
+    ];
     printFormattedBoard(b);
-    console.log(isEmpty(b));
-    console.log(isFull(b));
-    console.log(getAvailableMoves(b));
+    console.log(isTerminal(b));
     return (
         <GradientBackground>
             <SafeAreaView style={styles.container}>
