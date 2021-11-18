992e23a89d9cec5371961bebec6ee5f2cfaab3b6
Ceci007 <cecibenitezca@gmail.com>
2021-11-18 12:06:24 -0300

    Adding some sounds

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,16 +1,10 @@
-import React, { ReactElement, useState, useEffect } from "react";
+import React, { ReactElement, useState, useEffect, useRef } from "react";
 import { SafeAreaView } from "react-native";
-import styles from "./single-player-game.styles";
 import { GradientBackground, Board } from "@components";
-import {
-    printFormattedBoard,
-    BoardState,
-    isEmpty,
-    isFull,
-    getAvailableMoves,
-    isTerminal,
-    getBestMove
-} from "@utils";
+import styles from "./single-player-game.styles";
+import { isEmpty, BoardState, isTerminal, getBestMove, Cell } from "@utils";
+import { Audio } from "expo-av";
+import * as Haptics from "expo-haptics";
 
 export default function Game(): ReactElement {
     // prettier-ignore
@@ -19,41 +13,96 @@ export default function Game(): ReactElement {
         null, null, null,
         null, null, null
     ]);
-
     const [turn, setTurn] = useState<"HUMAN" | "BOT">(Math.random() < 0.5 ? "HUMAN" : "BOT");
     const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
+    const pop1SoundRef = useRef<Audio.Sound | null>(null);
+    const pop2SoundRef = useRef<Audio.Sound | null>(null);
+    const winSoundRef = useRef<Audio.Sound | null>(null);
+    const lossSoundRef = useRef<Audio.Sound | null>(null);
+    const drawSoundRef = useRef<Audio.Sound | null>(null);
+
     const gameResult = isTerminal(state);
 
     const insertCell = (cell: number, symbol: "x" | "o"): void => {
         const stateCopy: BoardState = [...state];
-
         if (stateCopy[cell] || isTerminal(stateCopy)) return;
-
         stateCopy[cell] = symbol;
         setState(stateCopy);
+        try {
+            symbol === "x"
+                ? pop1SoundRef.current?.replayAsync()
+                : pop2SoundRef.current?.replayAsync();
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     const handleOnCellPressed = (cell: number): void => {
-        if (turn !== "HUMAN") return;
+        if (turn != "HUMAN") return;
         insertCell(cell, isHumanMaximizing ? "x" : "o");
         setTurn("BOT");
     };
 
+    const getWinner = (winnerSymbol: Cell): "HUMAN" | "BOT" | "DRAW" => {
+        if (winnerSymbol === "x") {
+            return isHumanMaximizing ? "HUMAN" : "BOT";
+        }
+
+        if (winnerSymbol === "o") {
+            return isHumanMaximizing ? "BOT" : "HUMAN";
+        }
+
+        return "DRAW";
+    };
+
     useEffect(() => {
         if (gameResult) {
-            alert("Game over");
+            const winner = getWinner(gameResult.winner);
+
+            if (winner === "HUMAN") {
+                try {
+                    winSoundRef.current?.replayAsync();
+                    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+                } catch (error) {
+                    console.log(error);
+                }
+
+                alert("You won!");
+            }
+
+            if (winner === "BOT") {
+                try {
+                    lossSoundRef.current?.replayAsync();
+                    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+                } catch (error) {
+                    console.log(error);
+                }
+
+                alert("You lost!");
+            }
+
+            if (winner === "DRAW") {
+                try {
+                    drawSoundRef.current?.replayAsync();
+                    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
+                } catch (error) {
+                    console.log(error);
+                }
+
+                alert("it's a draw!");
+            }
         } else {
             if (turn === "BOT") {
                 if (isEmpty(state)) {
                     const centerAndCorners = [0, 2, 6, 8, 4];
                     const firstMove =
                         centerAndCorners[Math.floor(Math.random() * centerAndCorners.length)];
-
                     insertCell(firstMove, "x");
                     setIsHumanMaximizing(false);
                     setTurn("HUMAN");
                 } else {
-                    const best = getBestMove(state, !isHumanMaximizing, 0, -1);
+                    const best = getBestMove(state, !isHumanMaximizing, 0, 1);
                     insertCell(best, isHumanMaximizing ? "o" : "x");
                     setTurn("HUMAN");
                 }
@@ -61,11 +110,42 @@ export default function Game(): ReactElement {
         }
     }, [state, turn]);
 
+    useEffect(() => {
+        // load sounds
+        const pop1SoundObject = new Audio.Sound();
+        const pop2SoundObject = new Audio.Sound();
+        const winSoundObject = new Audio.Sound();
+        const lossSoundObject = new Audio.Sound();
+        const drawSoundObject = new Audio.Sound();
+
+        const loadSounds = async () => {
+            /* eslint-disable @typescript-eslint/no-var-requires */
+            await pop1SoundObject.loadAsync(require("@assets/pop_1.wav"));
+            pop1SoundRef.current = pop1SoundObject;
+            await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
+            pop2SoundRef.current = pop2SoundObject;
+            await winSoundObject.loadAsync(require("@assets/win.mp3"));
+            winSoundRef.current = winSoundObject;
+            await lossSoundObject.loadAsync(require("@assets/loss.mp3"));
+            lossSoundRef.current = lossSoundObject;
+            await drawSoundObject.loadAsync(require("@assets/draw.mp3"));
+            drawSoundRef.current = drawSoundObject;
+        };
+        loadSounds();
+        return () => {
+            // unload sounds
+            pop1SoundObject && pop1SoundObject.unloadAsync();
+            pop2SoundObject && pop2SoundObject.unloadAsync();
+            winSoundObject && winSoundObject.unloadAsync();
+            lossSoundObject && lossSoundObject.unloadAsync();
+            drawSoundObject && drawSoundObject.unloadAsync();
+        };
+    }, []);
     return (
         <GradientBackground>
             <SafeAreaView style={styles.container}>
                 <Board
-                    disabled={Boolean(isTerminal(state)) || turn !== "HUMAN"}
+                    disabled={Boolean(isTerminal(state)) || turn != "HUMAN"}
                     onCellPressed={cell => {
                         handleOnCellPressed(cell);
                     }}
