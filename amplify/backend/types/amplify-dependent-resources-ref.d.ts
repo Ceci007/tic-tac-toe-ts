5af5eb68f4c523e1403417d6eef25a96e4fe1b87
Ceci007 <cecibenitezca@gmail.com>
2021-11-30 12:36:05 -0300

    Adding TextInput component and Login screen

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,7 @@
 export type AmplifyDependentResourcesAttributes = {
     api: {
         tictactoe: {
+            GraphQLAPIKeyOutput: "string";
             GraphQLAPIIdOutput: "string";
             GraphQLAPIEndpointOutput: "string";
         };
---
+++ src/screens/login/login.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement, useRef } from "react";
+import { ScrollView, TextInput as NativeTextInput } from "react-native";
+import { GradientBackground, TextInput } from "@components";
+import styles from "./login.styles";
+
+export default function Login(): ReactElement {
+    const passwordRef = useRef<NativeTextInput | null>(null);
+
+    return (
+        <GradientBackground>
+            <ScrollView contentContainerStyle={styles.container}>
+                <TextInput
+                    returnKeyType="next"
+                    placeholder="Username"
+                    style={{ marginBottom: 20 }}
+                    onSubmitEditing={() => {
+                        passwordRef.current?.focus();
+                    }}
+                />
+                <TextInput
+                    ref={passwordRef}
+                    returnKeyType="done"
+                    secureTextEntry
+                    placeholder="Password"
+                />
+            </ScrollView>
+        </GradientBackground>
+    );
+}
