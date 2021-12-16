fe54a86dd57af400a011d239c39d4dfcdf3641ea
Ceci007 <cecibenitezca@gmail.com>
2021-12-16 15:54:13 -0300

    Correcting wrong path in index.js

--- src/screens/multiplayer-home/multiplayer-home.tsx
+++ src/screens/multiplayer-home/multiplayer-home.tsx
@@ -1,11 +1,13 @@
-import React, { ReactElement, useEffect } from "react";
-import { ScrollView, View, Alert } from "react-native";
+import React, { useEffect, ReactElement } from "react";
+import { ScrollView, View } from "react-native";
+import { Alert } from "react-native";
 import { API, graphqlOperation } from "aws-amplify";
+
 import { GradientBackground, Text } from "@components";
+import styles from "./multiplayer-home.styles";
 import { useAuth } from "@contexts/auth-context";
 import { colors } from "@utils";
 import { getPlayer } from "./multiplayer-home.graphql";
-import styles from "./multiplayer-home.styles";
 
 export default function MultiplayerHome(): ReactElement {
     const { user } = useAuth();
@@ -17,12 +19,13 @@ export default function MultiplayerHome(): ReactElement {
                     graphqlOperation(getPlayer, {
                         username: user.username,
                         limit: 1,
-                        sortDirection: "DESC",
-                        nextToken: nextToken
+                        nextToken: nextToken,
+                        sortDirection: "DESC"
                     })
                 );
-                console.log(player);
+                console.log("resp: ", player);
             } catch (error) {
+                console.log(error);
                 Alert.alert("Error!", "An error has occurred!");
             }
         }
